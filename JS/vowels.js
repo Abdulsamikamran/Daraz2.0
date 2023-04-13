7915b6bb356822ecb3461f8ac393a5236a4e5ab9
Abdulsamikamran <asamikamran@gmail.com>
2023-04-13 12:48:06 +0500

    vowels

---
+++ JS/vowels.js
@@ -0,0 +1,17 @@
+const input = prompt('enter string :');
+
+function vowel_count(input)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+   prompt('vcount');
+}
